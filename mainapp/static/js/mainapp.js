e11fe19d0b1a6d0add92e114e7225da164352bf2
vellimir <kkazancov@gmail.com>
2024-02-06 23:33:54 +0300

    models services, tag

--- mainapp/static/js/mainapp.js
+++ mainapp/static/js/mainapp.js
@@ -1,17 +1,27 @@
-const userIcon = document.querySelector('.wrapper-burger'),
-      userModal = document.querySelector('header');
+const userIcon = document.querySelector('.user'),
+      userModal = document.querySelector('.modal_user');
+
+    userIcon.addEventListener('click', () => {
+        userModal.style.display = (userModal.style.display == 'none') ? 'block' : 'none'
+    })
+
+    document.addEventListener('click', event => {
+        const targetElement = event.target;
+
+        if (!userModal.contains(targetElement) && !userIcon.contains(targetElement)) {
+            userModal.style.display = 'none';
+        }
+    });
 
 
-userIcon.addEventListener('click', () => {
-    userModal.style.display = (userModal.style.display == 'none') ? 'block' : 'none'
-});
+let header = document.querySelector('header'),
+    headerH = document.querySelector('header').clientHeight,
+    social = document.querySelector('.social');
 
-
-document.addEventListener('click', event => {
-    const targetElement = event.target;
-
-    if (!userModal.contains(targetElement) && !userIcon.contains(targetElement)) {
-        userModal.style.display = 'none';
+    document.onscroll = function () {
+        let scroll = window.scrollY
+        
+        if (scroll > headerH) {header.classList.add('active_header')}
+        
+        else {header.classList.remove('active_header')}
     }
-});
-
